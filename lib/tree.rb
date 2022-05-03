14a6ef8c601ca8d90fbb4aab9036dd480de4e15e
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-05-03 11:45:39 -0700

    commit tree 37d639a815c5673eb915becc7973ffbe40e53eb3

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,28 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      current = nodes_to_visit.pop
+      if current[:id] === id
+        return current
+      end
+
+      nodes_to_visit = nodes_to_visit + current[:children]
+    end
+
+    nil
+  end
+
+
+  # recursive version of depth first
+  def depth_first_traversal(node, result=[])
+    result.push(node[:value])
+    node[:children].each do |child|
+      depth_first_traversal(child, result)
+    end
+  end
 end
